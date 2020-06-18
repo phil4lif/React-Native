bb03f09cb715008a049275a8e5129b617418b9cb
phil cowan <philipjcowan1@gmail.com>
2020-06-17 17:57:51 -0700

    refactor the counter demo to use a reducer hook instead of usestate

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,18 +1,30 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+
+
+const reducer = (state, action) => {
+        //state = counter
+        //action = {type: increase || decrease, payload: 1}
+        switch (action.type) {
+            case 'increase':
+                return {...state, counter: state.counter + action.payload};
+            case 'decrease':
+                return {...state, counter: state.counter - action.payload};
+            default:
+                return state;
+        }
+}
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, {counter: 0})
+    const { counter } = state;
     return (
         <View>
             <Button title="Increase" 
-            onPress={() => {
-                setCounter(counter + 1);
-            }} />
+            onPress={() => dispatch({ type: 'increase', payload: 1 })}
+             />
             <Button title="Decrease" 
-            onPress={() => {
-                setCounter(counter - 1);
-            }}/>
+            onPress={() => dispatch({ type: 'decrease', payload: 1 })}/>
 
             <Text>Current Count: {counter}</Text>
         </View>
