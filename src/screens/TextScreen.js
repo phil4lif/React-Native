b4d73fb9729afe4f92f511471683b88d3250598a
phil cowan <philipjcowan1@gmail.com>
2020-06-18 13:37:02 -0700

    adds password to text screen with minimum 6char req

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('')
+
     return (
         <View>
             <TextInput 
             style={styles.input}
             autoCapitalize="none"
             autoCorrect={false}
-            placeholder="name"/>
+            placeholder="name"
+            value={name}
+            onChangeText={(newValue) => setName(newValue)}/>
+            <Text>my name is {name}</Text>
+            <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            placeholder="password"
+            autoCorrect={false}
+            value={password}
+            onChangeText={(newValue) => setPassword(newValue)}
+            />
+            {password.length < 6 ? <Text>Password must be at least 6 characters</Text> : <Text>Password is valid</Text>}
         </View>
     )
 }
