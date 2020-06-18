2bdf6a935f1738158b5406e5d4ac3181024475e7
phil cowan <philipjcowan1@gmail.com>
2020-06-17 17:27:10 -0700

    adds the color changing functionality back in

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -18,25 +18,21 @@ const reducer = (state, action) => {
     }
 };
 const SquareScreen = () => {
-
-
-
     const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0})
-    
-
+    const { red, green, blue } = state;
     return (
         <View>
             <ColorCounter
-                onIncrease={() => }
-                onDecrease={() => }
+                onIncrease={() => dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT})}
+                onDecrease={() => dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT})}
                 color="Red" />
             <ColorCounter
-                onIncrease={() => }
-                onDecrease={() => }
+                onIncrease={() => dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT})}
+                onDecrease={() => dispatch({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT})}
                 color="Blue" />
             <ColorCounter
-                onIncrease={() => }
-                onDecrease={() => }
+                onIncrease={() => dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT})}
+                onDecrease={() => dispatch({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT}) }
                 color="Green" />
             <View style={{ height: 150, width: 150, backgroundColor: `rgb(${red},${green},${blue})` }} />
         </View>
